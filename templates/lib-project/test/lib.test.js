5c9331d6594f014a31521f1e3e1561f7464d3b10
Phil Tomlinson <ptomlinson@quartileone.com>
2019-04-15 16:21:21 +1000

    Updated library project

---
+++ templates/lib-project/test/lib.test.js
@@ -0,0 +1,11 @@
+import { describe } from 'riteway'
+
+describe('Library', async (assert) => {
+      assert({
+        given: 'the library',
+        should: 'Should do something',
+        actual: 'True',
+        expected: 'True'
+      })
+    }
+)
